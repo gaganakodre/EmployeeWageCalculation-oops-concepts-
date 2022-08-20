e60cc66f5920fe2448713104701f16eb4a0b40a5
shreegowri <gaganandh@gmail.com>
2022-08-20 23:26:16 +0530

    [shreegowri] aclculating the monthly wage by giving some condition to it

--- employeewage_calculation.py
+++ employeewage_calculation.py
@@ -6,29 +6,31 @@ logging.basicConfig(filename='employee.log', level='DEBUG', format='%(asctime)s:
 
 
 class Employee:
-    def __init__(self, total_hr, monthly_wage, daily_wage, total_working_hr):
+    def __init__(self, total_hr, daily_wage, max_working_hr, max_working_days):
         self.total_hr = total_hr
-        self.monthly_wage = monthly_wage
         self.daily_wage = daily_wage
-        self.total_working_hr = total_working_hr
+        self.max_working_hr = max_working_hr
+        self.max_working_days = max_working_days
 
-    def employee_wage(self):
+    def employee_wage(self, monthly_wage=0):
         try:
-            while self.total_working_hr <= 20:
+            while self.max_working_days <= 20 and self.max_working_hr <= 100:
                 emp_check = random.randint(0, 2)
                 if emp_check == 1:
                     self.total_hr = 8
-                    #logging.debug('employee present')
+                    # logging.debug('employee present')
                 elif emp_check == 2:
                     self.total_hr = 4
-                    #logging.debug('employee in part-time')
+                    # logging.debug('employee in part-time')
                 else:
                     self.total_hr = 0
-                    #logging.debug('employee absent')
+                    # logging.debug('employee absent')
                 self.daily_Wage = int(20 * self.total_hr)
                 # logging.debug("dailywage: {}".format(self.daily_Wage))
-            self.monthly_wage += self.daily_Wage
-            logging.debug('monthly wage: {}'.format(self.monthly_wage))
+                monthly_wage +=self.daily_Wage #self.daily_Wage
+                self.max_working_days+=1
+                self.max_working_hr+=1
+            logging.debug('monthly wage: {}'.format(monthly_wage))
         except Exception as e:
             logging.error(e)
         finally:
@@ -36,5 +38,5 @@ class Employee:
 
 
 if __name__ == '__main__':
-    employee = Employee(0, 0, 0,0)
+    employee = Employee(0, 0, 0, 0)
     employee.employee_wage()
